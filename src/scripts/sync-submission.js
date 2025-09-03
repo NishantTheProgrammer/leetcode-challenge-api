2e41681426a5d1e2c3b7f47df70cec797de139f0
NishantTheProgrammer <knishant113@gmail.com>
2025-09-03 17:45:58 +0530

    able to get submission data

--- src/scripts/sync-submission.js
+++ src/scripts/sync-submission.js
@@ -1,22 +1,89 @@
 import mongoose from 'mongoose';
+import axios from 'axios';
 
 import '../../database.js';
-
-import Submission from '../model/submission.js';
+import Season from '../model/season.js';
 
 const sync = async () => {
-    await Submission.create({
-        "userId": "68b6c5f0fc8f6f1cd04a1386",
-        "seasonId": "68b1975f29ff283da961b5dd",
-        "participantId": "68b6ccc31dddfb024713f643",
-        "title": "Dynamic Programming Challenge",
-        "titleSlug": "dynamic-programming-challenge",
-        "submittedAt": "2025-09-02T10:30:00.000Z",
-        "language": "JavaScript",
-        "difficulty": "Hard"
-    });
-    console.log('Data inserted ✅');
+    const now = new Date();
+    const seasonsData = await Season.aggregate([
+        {
+            $match: {
+                startDate: { $lte: now },
+                endDate: { $gte: now }
+            }
+        },
+        {
+            $lookup: {
+                from: 'participants',
+                localField: '_id',
+                foreignField: 'seasonId',
+                as: 'participants'
+            }
+        },
+        {
+            $unwind: {
+                path: "$participants",
+                preserveNullAndEmptyArrays: true
+            }
+        },
+        {
+            $lookup: {
+                from: 'users', // users collection
+                localField: 'participants.userId', 
+                foreignField: '_id',
+                as: 'participants.user'
+            }
+        },
+        {
+            $unwind: {
+                path: "$participants.user",
+                preserveNullAndEmptyArrays: true
+            }
+        },
+        {
+            $group: {
+                _id: "$_id",
+                startDate: { $first: "$startDate" },
+                endDate: { $first: "$endDate" },
+                name: { $first: "$name" }, // if you have season name or other fields
+                participants: { $push: "$participants" }
+            }
+        }
+    ]);
+
+    for (const seasonData of seasonsData) {
+        await processSesionData(seasonData);
+    }
+
     mongoose.connection.close();
+};
+
+const processSesionData = async (seasonData) => {
+    const { name, startDate, endDate, participants } = seasonData;
+
+    console.log(`Fetching data (${name}): ${startDate.toLocaleString().split(',')[0]} to ${endDate.toLocaleString().split(',')[0]}`);
+    console.log('participants', participants[0]);
+    let payload = {
+        "query": `
+            query getMultipleACSubmissions { 
+
+                ${
+                    participants.map(({ user }) => `
+                        ${user.username}: recentAcSubmissionList(username: "${user.username}", limit: 2) { 
+                            id
+                            title 
+                            titleSlug 
+                            timestamp 
+                            statusDisplay 
+                            lang 
+                        } 
+                    `).join('\n')
+                } 
+            }`
+    };
+    const {data} = await axios.post('https://leetcode.com/graphql', payload);
+    console.log(JSON.stringify(data, 3, 3));
 }
 
 
